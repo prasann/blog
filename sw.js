9f7a3915b2138694bc8ddeace0ed7c9f75bad6ea
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2019-09-13 15:59:25 +0530

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/blog-gatsby/app-ac3a18bd43a598d390aa.js","b3249b1a0709d9b4dec86b19f8547768"],["/blog-gatsby/app-b6fda4890a5c8d837fc6.js","84efdc100366009c139121dd7fa12722"],["/blog-gatsby/commons-3f81b644373d89174c85.js","04c5be76f55519d1b57257fae32b90aa"],["/blog-gatsby/index.html","44dabbafc11371c9e88eb5924e1fc4a6"],["/blog-gatsby/manifest.webmanifest","67ec2737356b74aa6e2fb0537bbe917e"],["/blog-gatsby/offline-plugin-app-shell-fallback/index.html","39728478f605221286978de95f069e25"]];
+var precacheConfig = [["/app-5981259f472f44e5a760.js","f2c543478175a7ae04184d944c36a82c"],["/app-ac3a18bd43a598d390aa.js","b3249b1a0709d9b4dec86b19f8547768"],["/app-b6fda4890a5c8d837fc6.js","84efdc100366009c139121dd7fa12722"],["/commons-3508eeef5923faf6bb29.js","c1e096f08c96eb3bf3449035f4af8a77"],["/commons-3f81b644373d89174c85.js","04c5be76f55519d1b57257fae32b90aa"],["/index.html","18a69d330d5c57d718ebe0c72a91a728"],["/manifest.webmanifest","67ec2737356b74aa6e2fb0537bbe917e"],["/offline-plugin-app-shell-fallback/index.html","606c9febd6fea9c5602dbbfa7b88968a"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
